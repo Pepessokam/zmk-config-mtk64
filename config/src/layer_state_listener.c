70e6492bbb8d3cd61b67fd0396d52a73bc3c0c5b
masaomikamiya <man-machine@wind.email.ne.jp>
2025-04-19 00:21:05 +0900

    update

--- config/src/layer_state_listener.c
+++ config/src/layer_state_listener.c
@@ -8,9 +8,15 @@ int layer_state_changed_listener(const struct zmk_event_header *eh) {
     const struct layer_state_changed *ev = as_layer_state_changed(eh);
 
     // レイヤー番号と状態を UART に出力
-    printk("Layer %d %s\n", ev->layer, ev->state ? "on" : "off");
+    //printk("Layer %d %s\n", ev->layer, ev->state ? "on" : "off");
 
-    return 0;
+    //return 0;
+
+    // printkはタイミングによってはフリーズの原因に
+    // 代わりに軽量なログで出力確認（ログレベルはLOG_LEVEL_INF以上にする）
+    LOG_INF("Layer %d %s", ev->layer, ev->state ? "on" : "off");
+
+    return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(layer_notify, layer_state_changed_listener);
